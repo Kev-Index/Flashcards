6897a4a92128ade3f7d297c9ff3147945da9000e
Kevin Liu <kevinliu467@gmail.com>
2020-02-15 13:01:02 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Kevin Liu on 2/15/20.
+//  Copyright © 2020 Kevin Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
